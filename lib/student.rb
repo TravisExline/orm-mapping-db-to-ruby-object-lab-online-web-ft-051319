fb38569fd3da357125e595b591f2773a9ce82a71
Travis Exline <t.exline22@gmail.com>
2019-06-17 17:54:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,6 +79,17 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(number)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
